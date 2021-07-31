169a1f206cdc15d71c6b16589ff6ef5a74213294
Raj Karmakar <97rajmath@gmail.com>
2021-07-31 11:48:45 +0530

    Defined User model

---
+++ twitter-backend/models/Schema.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+
+let user={
+    id          :  {type:String,required:true,unique:true},
+    firstname   :  {type:String,required:true},
+    lastname    :  {type:String,required:true},
+    email       :  {type:String,required:true,unique:true},
+    password    :  {type:String,required:true,unique:true},
+    created_on  :  {type:Date,required:true},
+    permissions :  {type:Date,required:true},
+    posts       :  {type:Array,default:[]},
+    likes       :  {type:Array,default:[]},
+    comments    :  {type:Array,default:[]},
+    retweets    :  {type:Array,default:[]}
+};
--- twitter-backend/server.js
+++ twitter-backend/server.js
@@ -1,11 +1,13 @@
+const config = require('./config');
 const express=require('express');
 const cors=require('cors');
-const config = require('./config');
+const compression = require('compression');
 
 
 const app=express();
 
 app.use(cors());
+app.use(compression());
 
 app.listen(config.SERVER_PORT,()=>{
     console.log(`Server running on PORT:${config.SERVER_PORT}`);
