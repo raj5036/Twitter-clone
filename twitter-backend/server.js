9db4b9240a9ec8c439659d98c6f03ccf7f57183b
Raj Karmakar <97rajmath@gmail.com>
2021-07-31 13:11:08 +0530

    MongoDB Atlas setup

--- twitter-backend/server.js
+++ twitter-backend/server.js
@@ -1,8 +1,23 @@
+require('dotenv').config();
 const config = require('./config');
 const express=require('express');
 const cors=require('cors');
 const compression = require('compression');
+const mongoose=require('mongoose');
 
+mongoose.connect(`mongodb+srv://raj:${process.env.CLUSTER_ADMIN_PASSWORD}@cluster0.yufrz.mongodb.net/twitterDB`,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useCreateIndex:true,
+    useFindAndModify:false, 
+});
+
+//Check connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function() {
+  console.log(`Connected to DB`);
+});
 
 const app=express();
 
